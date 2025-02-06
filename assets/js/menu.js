202c2acebeb94a8126f4dbf5f1d057131a5832f0
Mateus Marques da Silva <mateus.marques.da.silva@gmail.com>
2025-02-06 19:27:59 -0300

    menu mobile criado, agora to ajustando a reponsividade

---
+++ assets/js/menu.js
@@ -0,0 +1,5 @@
+document.querySelector('.open_btn').addEventListener('click', () => {
+    document.querySelector('.MOBsidebar').classList.toggle('opemSidbar')
+    document.querySelector('.help').classList.toggle('openHelp')
+    
+})
